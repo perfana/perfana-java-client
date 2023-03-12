03bcfcc9aacd3daa45a773f010cfabc8da0a18c6
Peter Paul Bakker <peterpaul@perfana.io>
2023-03-12 14:07:56 +0100

    update license headers

--- src/main/java/io/perfana/client/PerfanaClientBuilder.java
+++ src/main/java/io/perfana/client/PerfanaClientBuilder.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2020-2022  Peter Paul Bakker @ Perfana.io, Daniel Moll @ Perfana.io
+ *    Copyright 2020-2023  Peter Paul Bakker @ perfana.io, Daniel Moll @ perfana.io
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
